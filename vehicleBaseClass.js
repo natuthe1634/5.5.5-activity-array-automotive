c393f40110ed9fb737eac5ec786ee790c77b6407
natuthe1634 <Princenatu22@gmail.com>
2023-04-19 18:22:39 -0700

    first atte

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -1,91 +1,67 @@
 class Vehicle {
     constructor(make, model, year, color, mileage) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.color = color;
-        this.passenger = 0;
-        this.speed = 0;
-        this.mileage = mileage;
-        this.started = false;
-        this.numberOfWheels = 0;
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.color = color;
+      this.passenger = 0;
+      this.speed = 0;
+      this.mileage = mileage;
+      this.started = false;
+      this.numberOfWheels = 0;
+      this.fuel = 100;
     }
-
-    start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
-        } else {
-            return this.started = false;
-            console.log("engine cannot start...");
-        }
-    }
+  
     accelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                console.log(this.speed += 1);
-                this.fuel = this.fuel - 1;
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
+      if (this.started) {
+        if (this.fuel > 0) {
+          console.log((this.speed += 1));
+          this.fuel = this.fuel - 1;
         } else {
-            alert("You need to start the engine first.");
+          console.log("out of fuel.");
+          this.stop();
         }
+      } else {
+        alert("You need to start the engine first.");
+      }
     }
+  
     decelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                if (this.speed > 0) {
-                    console.log(this.speed -= 1);
-                    this.fuel = this.fuel - 1;
-                } else {
-                    console.log(this + " has stopped moving");
-                    this.fuel = this.fuel - 1;
-                }
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
+      if (this.started) {
+        if (this.fuel > 0) {
+          if (this.speed > 0) {
+            console.log((this.speed -= 1));
+            this.fuel = this.fuel - 1;
+          } else {
+            console.log(this + " has stopped moving");
+            this.fuel = this.fuel - 1;
+          }
         } else {
-            alert("You need to start the engine first.");
+          console.log("out of fuel.");
+          this.stop();
         }
+      } else {
+        alert("You need to start the engine first.");
+      }
     }
+  
     stop() {
-        this.started = false;
+      console.log("engine off");
+      this.started = false;
     }
-
-    drive() {
-        accelerate();
+  
+    typeOfVehicle() {
+      if (this.numberOfWheels == 8) {
+        console.log(this.model + " " + this.make + " is a Truck");
+      } else if (this.numberOfWheels == 4) {
+        console.log(this.model + " " + this.make + " is a Car");
+      } else if (this.numberOfWheels == 2) {
+        console.log(this.model + " " + this.make + " is a Bike");
+      } else {
+        console.log("Unknown type of vehicle");
+      }
     }
-    brake() {
-        decelerate();
-    }
-
-    autoPark()
-    {
-
-    }
-
-    autoDrive()
-    {
-      
-    }
-
-    typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
-            console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
-            console.log(this.model + " " + this.make + " is a Bike");
-        } else {
-            console.log("Unknown type of vehicle");
-        }
-    }
-}
-
-//This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+  }
+  
+  //This exports things you want to use from this "module", more info in readme
+  module.exports = { Vehicle };
